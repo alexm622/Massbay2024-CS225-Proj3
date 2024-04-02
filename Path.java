7ec6fde96b42608b7ebc3dab601aa3940b8c10a3
ian chung <130524551+2annnnnnnn@users.noreply.github.com>
2024-04-02 11:00:22 -0400

    Update Path.java

--- Path.java
+++ Path.java
@@ -1,2 +1,13 @@
-public class Path {
+public class Path{
+
+    private Car[] cars;
+    private Location[] locations;
+
+    public Path(int numberofCars, int numberofLocations){
+
+        this.cars = new Car[numberofCars];
+        this.locaitons = new Checkpoint[numberofLocations];
+
+    }
+
 }
