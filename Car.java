6311fe0b3203b1ce2378b5209fc11f5b6d5a5211
ian chung <130524551+2annnnnnnn@users.noreply.github.com>
2024-04-02 10:59:44 -0400

    Update Car.java

--- Car.java
+++ Car.java
@@ -1,49 +1,27 @@
-import java.awt.Color;
+public class Car{
 
-public class Car {
-    private int carNumber;
     private String carName;
     private Color carColor;
-    private int carSpeed;
+    private double carSpeed;
+    private Location currentLocation;
 
-    public Car(int carNumber, String carName, Color carColor, int carSpeed) {
-        this.carNumber = carNumber;
+    public Car(String carName, Color carColor, double carSpeed, Location locaiton){
         this.carName = carName;
         this.carColor = carColor;
         this.carSpeed = carSpeed;
+        this.currentLocation = location;
     }
 
-
-    public int getCarNumber() {
-        return this.carNumber;
+    public Location getCurrentlocation(){
+        return this.currentLocation;
     }
 
-    public void setCarNumber(int carNumber) {
-        this.carNumber = carNumber;
-    }
-
-    public String getCarName() {
+    public String getcarName(){
         return this.carName;
     }
-
-    public void setCarName(String carName) {
-        this.carName = carName;
-    }
-
-    public Color getCarColor() {
-        return this.carColor;
-    }
-
-    public void setCarColor(Color carColor) {
-        this.carColor = carColor;
-    }
-
-    public int getCarSpeed() {
-        return this.carSpeed;
-    }
-
-    public void setCarSpeed(int carSpeed) {
-        this.carSpeed = carSpeed;
-    }
     
+    public double getcarSpeed(){
+        return carSpeed
+    }
+
 }
